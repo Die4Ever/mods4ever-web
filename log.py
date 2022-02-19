381a76403bfd2618fb3c7c9bb522eef42e52e439
Die4Ever <die4ever2005@gmail.com>
2022-02-19 17:23:41 -0600

    parsing content

--- log.py
+++ log.py
@@ -84,7 +84,10 @@ def write_db(version, ip, content):
 	try:
 		create_tables(db)
 		cursor = db.cursor()
-		cursor.execute("INSERT INTO logs SET created=NOW(), version=%s, ip=%s, message=%s", (version, ip, content))
+		d = parse_content(content)
+		cursor.execute("INSERT INTO logs SET "
+						+ "created=NOW(), version=%s, ip=%s, message=%s, map=%s, seed=%s, flagshash=%s",
+												(version, ip, content, d['map'], d['seed'], d['flagshash']))
 		log_id = cursor.lastrowid
 		info("inserted logs id "+str(log_id))
 		for d in get_deaths(content):
@@ -100,9 +103,26 @@ def write_db(version, ip, content):
 	db.close()
 
 
+def parse_content(content):
+	d = {}
+	r = re.compile(r'^(?P<level>\w+): (?P<map>[^\.]*)\.(?P<module>[^:]+)\d+: ((?P<firstword>\w+) (?P<remaining>.*)$)?', flags=re.MULTILINE)
+	r2 = re.compile(r' (?P<key>\w+): (?P<value>[\w\d]+)')
+	for i in r.finditer(content):
+		try:
+			d = {**d, **i.groupdict()}
+			if 'remaining' in d and d['remaining'] is not None:
+				for j in r2.finditer(d['remaining']):
+					d[j.group('key')] = j.group('value')
+			d.pop('remaining', None)
+		except Exception as e:
+			logex(e)
+	return d
+
 def get_deaths(content):
 	deaths = []
-	r = re.compile(r'^DEATH: [^:]+: (?P<player>.*) was killed( by (?P<killerclass>.*?) (?P<killer>.*?) with (?P<dmgtype>.*?) damage)? in (?P<map>.*?) \((?P<x>.*?),(?P<y>.*?),(?P<z>.*?)\)', flags=re.MULTILINE)
+	r = re.compile(
+		r'^DEATH: [^:]+: (?P<player>.*) was killed( by (?P<killerclass>.*?) (?P<killer>.*?) with (?P<dmgtype>.*?) damage)? in (?P<map>.*?) \((?P<x>.*?),(?P<y>.*?),(?P<z>.*?)\)'
+		, flags=re.MULTILINE)
 	for i in r.finditer(content):
 		d = i.groupdict()
 		deaths.append(d)
@@ -206,6 +226,9 @@ def run_tests():
 	
 	for d in get_deaths("\nDEATH: 01_NYC_UNATCOIsland.JCDentonMale2: JC Denton was killed by SecurityBot3 UJ-31 with shot damage in 01_NYC_UNATCOISLAND (-502.167694,40.753559,-119.199997)\nDEATH: 01_NYC_UNATCOIsland.JCDentonMale2: Die4Ever was killed in 01_NYC_UNATCOISLAND (-502.167694,40.753559,-119.199997)"):
 		info(repr(d))
+	
+	info("testing parse_content")
+	parse_content("INFO: 01_NYC_UNATCOIsland.DXRMachines0: _SpawnNewActor 01_NYC_UNATCOIsland.DataCube12 at (6404.268066,4184.700195,-123.422623)\nINFO: 01_NYC_UNATCOIsland.DXRando12: done randomizing 01_NYC_UNATCOISLAND using seed 191616\nINFO: 01_NYC_UNATCOIsland.DXRFlags12: AnyEntry 01_NYC_UNATCOISLAND DeusEx.DXRFlags - v1.7.3.2 Alpha, seed: 191616, flagshash: 1192551168, playthrough_id: 1686588103, flagsversion: 1070302, gamemode: 0, difficulty: 1.500000, loadout: 0, brightness: 15, newgameplus_loops: 0, autosave: 0, crowdcontrol: 0, codes_mode: 2\nINFO: 01_NYC_UNATCOIsland.DXRFlags12: AnyEntry 01_NYC_UNATCOISLAND - ammo: 70, merchants: 30, minskill: 50, maxskill: 300, skills_disable_downgrades: 0, skills_reroll_missions: 5, skills_independent_levels: 0, multitools: 70, lockpicks: 70, biocells: 70, medkits: 70, speedlevel: 1, keysrando: 4, doorsmode: 259, doorspickable: 50, doorsdestructible: 50, deviceshackable: 100, passwordsrandomized: 100, enemiesrandomized: 30, enemyrespawn: 0, infodevices: 100, startinglocations: 100, goals: 100, equipment: 2, dancingpercent: 25, medbots: 25, repairbots: 25, medbotuses: 0, repairbotuses: 0, medbotcooldowns: 1, repairbotcooldowns: 1, medbotamount: 1, repairbotamount: 1, turrets_move: 50, turrets_add: 70, banned_skills: 5, banned_skill_levels: 5, enemies_nonhumans: 60, swapitems: 100, swapcontainers: 100, augcans: 100, aug_value_rando: 100, skill_value_rando: 100, min_weapon_dmg: 50, max_weapon_dmg: 150, min_weapon_shottime: 50, max_weapon_shottime: 150\nINFO: 01_NYC_UNATCOIsland.DXRTelemetry8: health: 100, HealthLegLeft: 100, HealthLegRight: 100, HealthTorso: 100, HealthHead: 100, HealthArmLeft: 100, HealthArmRight: 100")
 
 	info("path: "+os.path.dirname(os.path.realpath(__file__)))
 	info("cwd: "+os.getcwd())
