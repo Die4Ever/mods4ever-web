98553fa3d1943d1fc50d2a191a27988a0f0b3b57
Die4Ever <die4ever2005@gmail.com>
2022-08-05 11:57:19 -0500

    v2.1 update notification

--- dxlog/request.py
+++ dxlog/request.py
@@ -2,12 +2,12 @@ from dxlog.base import *
 
 def update_notification(mod, version):
 	response = {}
-	notification = "New v2.0.3 available!"
-	desc = "Improvements and bug fixes!"
-	detail = ""
+	notification = "New v2.1 available!"
+	desc = "Way more goal"
+	detail = "location randomization!"
 	assert len(desc) < 35
 	assert len(detail) < 35
-	if VersionStringToInt(version) < VersionToInt(2, 0, 3, 8):
+	if VersionStringToInt(version) < VersionToInt(2, 1, 0, 4):
 		response['notification'] = notification
 		response['message'] = desc
 		response['message'] += "|n" + detail
