e8a2222de486e13b95a35ef2be35a88ffa130dd3
Die4Ever <die4ever2005@gmail.com>
2022-12-19 17:40:52 -0600

    v2.2.1 update notification

--- dxlog/request.py
+++ dxlog/request.py
@@ -2,12 +2,12 @@ from dxlog.base import *
 
 def update_notification(mod, version):
 	response = {}
-	notification = "New v2.2 available!"
-	desc = "More goals randomization,"
-	detail = "VMD 1.57 support, and more!"
+	notification = "New v2.2.1 available!"
+	desc = "Spoiler buttons,"
+	detail = "and aug balance tweaks!"
 	assert len(desc) < 35
 	assert len(detail) < 35
-	if VersionStringToInt(version) < VersionToInt(2, 2, 0, 2):
+	if VersionStringToInt(version) < VersionToInt(2, 2, 1, 1):
 		response['notification'] = notification
 		response['message'] = desc
 		response['message'] += "|n" + detail
