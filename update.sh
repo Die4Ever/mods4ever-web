f92f1c792798c05b5a0380e4e6fb201ecc725ff9
Die4Ever <die4ever2005@gmail.com>
2023-09-15 04:23:20 -0500

    update.sh redirect output

--- update.sh
+++ update.sh
@@ -9,7 +9,7 @@ fi
 git pull
 python3 tests.py
 echo 'about to kill old process'
-pgrep -af 'python.*\s+web.py'
-pkill -f 'python.*\s+web.py'
+pgrep -af 'python.*\s+web.py' || echo "old process not found"
+pkill -f 'python.*\s+web.py' || echo "old process not found"
 echo 'starting server'
-python3 web.py &
+python3 web.py 2>/dev/null &
