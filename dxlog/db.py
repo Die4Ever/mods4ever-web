d1ad4df1e7c6d89d92f8869ebd54c655e86ddd27
Die4Ever <die4ever2005@gmail.com>
2024-05-15 20:01:27 -0500

    debug logging for BeatGame event

--- dxlog/db.py
+++ dxlog/db.py
@@ -101,6 +101,9 @@ def write_db(mod, version, ip, content:str, config, data):
 					if event['type'] == 'DEATH':
 						log_death(cursor, log_id, event)
 					if event["type"] == "BeatGame":
+						info('got BeatGame event')
+						info(content)
+						info(repr(event))
 						log_beatgame(cursor, log_id, mod, version, event, d)
 					if event['type'] == 'QueryLeaderboard':
 						ret.update(QueryLeaderboardGame(cursor, event, d.get('playthrough_id')))
