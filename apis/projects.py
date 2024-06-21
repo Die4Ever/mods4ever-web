ad8d284b0e66a9daa2e848f2f057ff8dd85c62af
Die4Ever <die4ever2005@gmail.com>
2024-06-21 08:04:56 -0500

    new dxrando v3.0 trailer

--- apis/projects.py
+++ apis/projects.py
@@ -4,7 +4,7 @@ def get_projects():
         'DXRando': {
             'name': 'Deus Ex Randomizer',
             'description': 'This is a mod for the original Deus Ex that takes everything and shuffles it all around to make it like a new game every time but with the same great story. The goal is to increase the replayability and strategy.',
-            'youtube': 'XsoIKbn_suE',
+            'youtube': 'V3mTcG6xeq4',
             'repo': 'Die4Ever/deus-ex-randomizer',
             'community': 'deus_ex_randomizer',
             'downloads': {
